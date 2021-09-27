43486223abbd0a95107fdeda34b32b9e19c5b7d7
Nicolas Sauzeat <nicolas.sauzeat@gmail.com>
2021-09-27 21:09:03 +0200

    Commit application 1 fonctionnalité

--- app.py
+++ app.py
@@ -5,25 +5,34 @@ import pandas as pd
 import fr_core_news_sm
 nlp = fr_core_news_sm.load()
 from spacy.lang.fr.stop_words import STOP_WORDS
+import streamlit as st
+import numpy as np
 
 def main():
-    methode = input("Veuillez sélectionner la méthode de prédiction : \n input \n upload \n     ")
-    methode = methode.lower().replace(" ","")
-    if methode=="input":
-        texte = input("Veuillez entrer un commentaire : \n")
-        lemma_texte = lemmatize_text(texte)
-        texte_final= text_preprocess(lemma_texte)
-        prediction= predict(texte_final)
-        result= resultat(prediction)
-    elif methode=="upload":
-        print("Cette méthode n'est pas encore utilisable")
-    else:
-        print("Veuillez choisir une méthode utilisable")
+    
+    optionSide = st.sidebar.selectbox(
+        'Summary',
+        ['Chargé','Copié/Collé'])
+
+
+    if optionSide == 'Chargé':
+     file = st.file_uploader("file", type = ["txt"])
+     if st.button("Process"):
+        if file is not None:
+            text = str(file.read(), "utf-8")
+    if (file != '') and (file != None):
+        st.write('Texte initial : ')
+        st.write(file)
+        text_lemma = lemmatize_text(file)
+        st.write('Resultat : ')
+        texte_preprocess = text_preprocess(text_lemma)
+        prediction= predict(texte_preprocess)
+        st.success(prediction)
 
 def lemmatize_text(texte):
     texte = "".join(ch for ch in texte if ch.isalnum() or ch==" ")
     texte = texte.replace(" +"," ").lower().strip()
-    title= nlp(title)
+    title= nlp(texte)
     title=" ".join([token.lemma_ for token in title if token.text not in STOP_WORDS])
     return title
 
@@ -32,9 +41,23 @@ def text_preprocess(lemma_texte):
     lemma_texte= " ".join([token.lemma_ for token in nlp(lemma_texte)])
     tokenizer= tf.keras.preprocessing.text.Tokenizer()
     tokenizer.fit_on_texts(lemma_texte)
-    tokenize_text= tokenizer.text_to_sequences([lemma_texte])
-    paddle_text= tf.keras.preprocessing.sequence.pad_sequences(title, maxlen=163, padding="post")
+    tokenize_text= tokenizer.texts_to_sequences([lemma_texte])
+    paddle_text= tf.keras.preprocessing.sequence.pad_sequences(tokenize_text, maxlen=248, padding="post")
     return paddle_text
 
 def predict(texte_final):
-    classifier = joblib.load(r"")
+    classifier = tf.keras.models.load_model("my_model.h5")
+    prediction = np.argmax(classifier.predict(texte_final))
+    probabilite = np.max(classifier.predict(texte_final) * 100).astype("float")
+    if probabilite <65 :
+        return prediction, 'Probabilité de prédiction faible'
+    elif probabilite < 90:
+        return prediction, 'Probabilité de prédiction élevée'
+    else :
+        return prediction, "Probabilité de prédiction très élevée"
+
+
+
+
+if __name__ == "__main__":
+       main()
